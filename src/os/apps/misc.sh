4fd3e19f8788b3a760e3eee4c1fdaaa3cf3ccf17
roula <roulaa6@gmail.com>
2019-07-20 21:45:28 +0400

    Install applications

---
+++ src/os/apps/misc.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd "$(dirname "${BASH_SOURCE[0]}")" \
+  && . "../utils.sh" \
+  && . "./utils.sh"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+print_in_purple "\n   Miscellaneous\n\n"
+
+brew_install "Spectacle" "spectacle" "caskroom/cask" "cask"
+brew_install "Unarchiver" "the-unarchiver" "caskroom/cask" "cask"
+brew_install "VLC" "vlc" "caskroom/cask" "cask"
